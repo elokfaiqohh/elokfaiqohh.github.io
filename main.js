e846b76aca3f0bec24f0cbae8223311c375864c8
elokfaiqohh <elokfaiqoh885@gmail.com>
2024-02-02 16:27:27 +0700

    upgrade the contact form

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
 // alert("Website ini masih dalam on building");
+const contactForm = document.getElementById("contact-form");
+const loader = document.querySelector(".loading");
+
+loader.style.display = "none";
+
+contactForm.addEventListener("submit", function(e){
+    loader.style.display = "block";
+    e.preventDefault();
+
+    const url = e.target.action;
+    const formData = new FormData(contactForm);
+
+    fetch(url, {
+        method: "POST",
+        body: formData,
+        mode: "no-cors",
+    })
+    .then(() =>{
+    //url thankyou
+        window.location.href = "thanks.html"
+        loader.style.display = "none";
+    })
+    .catch((e) => alert("Error!! Please refresh the home page"))
+})
