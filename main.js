3cdc29bc1ec2d0a710b05f42bb719a2c06173ee7
elokfaiqohh <elokfaiqoh885@gmail.com>
2024-06-27 16:38:00 +0700

    Updates

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
-
-// swal("This website is still under development.");
+swal("This website is still under development.");
 var typed = new Typed(".auto-type", {
     strings : ["Elok", "Student", "Programmer"],
     typeSpeed : 150,
     backSpeed : 150,
     loop : true
+})
+
+window.addEventListener('scroll', () =>{
+  document.querySelector('header').classList.toggle
+  ('window-scroll', window.scrollY > 700)
 })
