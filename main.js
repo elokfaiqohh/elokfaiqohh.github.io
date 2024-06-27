8f1656cce76bd91d0a7914a41ff2a24aaa9f38c4
elokfaiqohh <elokfaiqoh885@gmail.com>
2024-06-27 15:25:43 +0700

    Update

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 
+// swal("This website is still under development.");
+var typed = new Typed(".auto-type", {
+    strings : ["Elok", "Student", "Programmer"],
+    typeSpeed : 150,
+    backSpeed : 150,
+    loop : true
+})
