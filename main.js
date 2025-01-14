66a86246c4a384bd100ca8127bf2c2a41b3087ac
elokfaiqohh <elokfaiqoh885@gmail.com>
2025-01-14 17:17:23 +0700

    update finishing web

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-swal("This website is still under development.");
+// swal("This website is still under development.");
 var typed = new Typed(".auto-type", {
   strings: ["Elok", "Student", "Programmer"],
   typeSpeed: 150,
